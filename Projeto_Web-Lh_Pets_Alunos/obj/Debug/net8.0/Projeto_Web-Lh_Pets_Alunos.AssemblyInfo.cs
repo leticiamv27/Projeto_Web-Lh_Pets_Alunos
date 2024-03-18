7346f3a311a22f28d5b9be374b2ed6a69d2a20a3
leticiamv27 <leticiamv@gmail.com>
2024-03-18 20:19:46 -0300

    Ultimo projeto

--- Projeto_Web-Lh_Pets_Alunos/obj/Debug/net8.0/Projeto_Web-Lh_Pets_Alunos.AssemblyInfo.cs
+++ Projeto_Web-Lh_Pets_Alunos/obj/Debug/net8.0/Projeto_Web-Lh_Pets_Alunos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projeto_Web-Lh_Pets_Alunos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b90b636fc61be7e43f505767088fae83269451b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projeto_Web-Lh_Pets_Alunos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projeto_Web-Lh_Pets_Alunos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
